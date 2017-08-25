2a092691574b435629fbc1f68d5db069d90d4fc5
Sumit Saini <sainisumit123@gmail.com>
2017-08-25 16:45:59 +0530

    Registeration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,16 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+if ('serviceWorker' in navigator) {
+    console.log('Service Worker is supported');
+    navigator.serviceWorker.register('/serviceworker.js')
+        .then(function(registration) {
+            console.log('Successfully registered!', ':^)', registration);
+            registration.pushManager.subscribe({ userVisibleOnly: true })
+                .then(function(subscription) {
+                    console.log('endpoint:', subscription.endpoint);
+                });
+        }).catch(function(error) {
+        console.log('Registration failed', ':^(', error);
+    });
+}
